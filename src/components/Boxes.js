f0a26370847ac4b37aa5ac52583084e89adb94d8
n-katte <nikhilkatte7039@gmail.com>
2020-06-10 10:26:51 +0530

    minor changes

--- src/components/Boxes.js
+++ src/components/Boxes.js
@@ -7,23 +7,27 @@ import Footer from './Footer';
                 <div style={bg}>
         <div class="rows first">
         <div class="session">
-            <Link to="/blank">session</Link>
+        <img style={{height : "80px", width : "80px"}}  src={require("../assests/session.jpg")}></img>
+            <Link to="/blank">Session</Link>
             
            
           </div>
           <div class="practice">
+          <img style={{height : "80px", width:"80px"}}  src={require("../assests/quest.jpg")}></img>
             <a>practice</a>
             
           </div>
         </div>
         <div class="rows first">
         <div class="downloads">
-            <Link to="/downloads"> <a>downloads</a></Link>
+        <img style={{height : "50px" ,width: "50px"}}  src={require("../assests/download.jpg")}></img>
+            <Link to="/downloads"><a>Downloads</a></Link>
            
            
           </div>
           <div class="ask">
-            <a>ask</a>
+          <img style={{height : "80px",width: "80px"}}  src={require("../assests/pad.jpg")}></img> 
+          <a>Ask</a>
            
           </div>
         </div>
@@ -37,7 +41,7 @@ import Footer from './Footer';
 
 const bg = {
     backgroundColor: "#6b9c9b",
-    height: "60vh",
+    height: "80vh",
     marginLeft: "360px",
     border: "1px solid black",
   };
